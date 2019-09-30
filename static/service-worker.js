4760f32f3d005a85142b614842c15198ec9bd961
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-30 19:19:06 +0530

    Service worker fixes
    Remove logging and run cache purge in activate event
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- static/service-worker.js
+++ static/service-worker.js
@@ -32,12 +32,7 @@ const precacheResources = [
   '/posts/teaching-kotlin-variables/index.html'
 ]
 
-for (id in oldCaches) {
-  self.caches.delete(id)
-}
-
 self.addEventListener('install', event => {
-  console.log('Service worker install event!')
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(precacheResources)
@@ -46,11 +41,12 @@ self.addEventListener('install', event => {
 })
 
 self.addEventListener('activate', event => {
-  console.log('Service worker activate event!')
+  for (id in oldCaches) {
+    self.caches.delete(id)
+  }
 })
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted for:', event.request.url)
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
       if (cachedResponse) {
