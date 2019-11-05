adb9e065157cab65302034d7693a895f9d472b91
Harsh Shandilya <msfjarvis@gmail.com>
2019-11-05 22:33:14 +0530

    Setup staging deployment
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ ci/deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+export SSHDIR="$HOME/.ssh"
+mkdir -p "$SSHDIR"
+echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
+chmod 600 "$SSHDIR/key"
+export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
+cd "$GITHUB_WORKSPACE/public"
+rsync -ahvcr --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING"
