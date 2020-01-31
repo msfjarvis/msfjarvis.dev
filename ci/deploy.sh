8eae674bcdd539747d24367fc34b17dd7cc9eeab
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-31 21:52:26 +0530

    Revert "ci: Tweak deployment pipeline"
    This reverts commit 75064df8cf8ca62e000eb867f22765a0a4d5210c.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,5 +6,4 @@ echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
 chmod 600 "$SSHDIR/key"
 export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
 cd "$GITHUB_WORKSPACE/public"
-rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING" || true
-exit 0
+rsync -ahvcr --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING"
