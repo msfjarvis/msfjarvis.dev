284e2e4dd5c28e17f04410826a3d42e66debea2f
Harsh Shandilya <me@msfjarvis.dev>
2020-08-14 02:45:32 +0530

    scripts/openring: import feed list from my TT-RSS instance
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- scripts/openring.sh
+++ scripts/openring.sh
@@ -3,5 +3,12 @@ openring \
   -s https://stjepang.github.io/feed.xml \
   -s https://fasterthanli.me/index.xml \
   -s https://deterministic.space/feed.xml \
+  -s https://adambennett.dev/index.xml \
+  -s https://chris.banes.dev/atom.xml \
+  -s https://jakewharton.com/feed.xml \
+  -s https://joebirch.co/feed/ \
+  -s https://publicobject.com/rss/ \
+  -s https://mrandri19.github.io/feed.xml \
+  -s https://blog.jessfraz.com/index.xml \
   < openring/in.html \
   > layouts/partials/openring.html
