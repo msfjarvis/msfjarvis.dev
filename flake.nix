5688ca352db49c97a4d87314b2fbc1c5d65c7587
Harsh Shandilya <me@msfjarvis.dev>
2022-11-16 17:27:38 +0530

    feat(nix): migrate to Flakes

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "The source behind msfjarvis.dev";
+
+  inputs = {
+    devshell.url = "github:numtide/devshell/master";
+    flake-utils.url = "github:numtide/flake-utils/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, devshell, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
+      in {
+        devShell = pkgs.devshell.mkShell {
+          name = "blog-dev-shell";
+          bash = { interactive = ""; };
+          packages = with pkgs; [
+            deno
+            git
+            go
+            hugo
+            imagemagick
+            nodejs-16_x
+            yarn
+          ];
+          commands = [{
+            name = "dev";
+            category = "development";
+            command = "${pkgs.yarn}/bin/yarn run dev";
+          }];
+        };
+      });
+}
