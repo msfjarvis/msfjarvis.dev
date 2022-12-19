4fd7239f837505b3fd94c539f00bcbc9798da098
Harsh Shandilya <me@msfjarvis.dev>
2022-12-19 10:31:08 +0530

    chore: replace imagemagick with libwebp

--- scripts/pngtowebp.sh
+++ scripts/pngtowebp.sh
@@ -2,5 +2,5 @@
 
 set -euxo pipefail
 
-fd -tf png$ static/uploads -x convert {} -quality 100 -define webp:lossless=true {.}.webp
+fd -tf png$ static/uploads -x cwebp -lossless -mt {} -o {.}.webp
 fd -tf png$ static/uploads -X rm -v
