9fcf30ea0324af1fb2381806b67d720ca3373622
Harsh Shandilya <me@msfjarvis.dev>
2024-03-31 21:56:49 +0530

    fix script

--- scripts/pngtowebp.sh
+++ scripts/pngtowebp.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
-fd -tf png$ static/uploads -x cwebp -lossless -mt {} -o {.}.webp
-fd -tf png$ static/uploads -X rm -v
+function convert_to_webp() {
+  local DIR
+  DIR="${1}"
+  fd -tf png$ "${DIR:?}" -x cwebp -lossless -mt {} -o '{.}.webp'
+  fd -tf png$ "${DIR:?}" -X rm -v
+}
+
+convert_to_webp static/uploads
+convert_to_webp content/posts
