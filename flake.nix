7257ee93759adf60c2238e73fbb873d40076d907
Harsh Shandilya <me@msfjarvis.dev>
2024-08-01 16:42:44 +0530

    chore: remove now-unused input

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
 
   inputs.devshell.url = "github:numtide/devshell";
   inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.devshell.inputs.flake-utils.follows = "flake-utils";
 
   inputs.flake-compat.url = "github:nix-community/flake-compat";
   inputs.flake-compat.flake = false;
