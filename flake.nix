f6dc17418e473a6c877e2a04b8d66e22b54db84c
Harsh Shandilya <me@msfjarvis.dev>
2024-11-03 18:20:35 +0530

    chore: format Nix files

--- flake.nix
+++ flake.nix
@@ -14,115 +14,122 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-utils.inputs.systems.follows = "systems";
 
-  outputs = {
-    self,
-    devshell,
-    flake-utils,
-    nixpkgs,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [devshell.overlays.default];
-      };
-    in {
-      devShell = pkgs.devshell.mkShell {
-        name = "blog-dev-shell";
-        bash = {interactive = "";};
-        packages = with pkgs; [
-          dprint
-          git
-          go
-          hugo
-          hyperlink
-          libwebp
-        ];
-        commands = [
-          {
-            name = "build";
-            category = "deployment";
-            command = "hugo";
-            help = "Build the site";
-          }
-          {
-            name = "conv";
-            category = "development";
-            command = ''
-              DIR=content/posts
-              fd -tf png$ "$DIR" -x cwebp -lossless -mt {} -o '{.}.webp'
-              fd -tf png$ "$DIR" -X rm -v
-            '';
-            help = "Convert all PNGs to WebP";
-          }
-          {
-            name = "dev";
-            category = "development";
-            command = "hugo server -D";
-            help = "Run the Hugo development server";
-          }
-          {
-            name = "format";
-            category = "development";
-            command = "dprint fmt";
-            help = "Format this repository";
-          }
-          {
-            name = "diffs";
-            category = "development";
-            command = ''
-              build
-              mv public new
-              git stash
+  outputs =
+    {
+      self,
+      devshell,
+      flake-utils,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlays.default ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          name = "blog-dev-shell";
+          bash = {
+            interactive = "";
+          };
+          packages = with pkgs; [
+            dprint
+            git
+            go
+            hugo
+            hyperlink
+            libwebp
+          ];
+          commands = [
+            {
+              name = "build";
+              category = "deployment";
+              command = "hugo";
+              help = "Build the site";
+            }
+            {
+              name = "conv";
+              category = "development";
+              command = ''
+                DIR=content/posts
+                fd -tf png$ "$DIR" -x cwebp -lossless -mt {} -o '{.}.webp'
+                fd -tf png$ "$DIR" -X rm -v
+              '';
+              help = "Convert all PNGs to WebP";
+            }
+            {
+              name = "dev";
+              category = "development";
+              command = "hugo server -D";
+              help = "Run the Hugo development server";
+            }
+            {
+              name = "format";
+              category = "development";
+              command = "dprint fmt";
+              help = "Format this repository";
+            }
+            {
+              name = "diffs";
+              category = "development";
+              command = ''
+                build
+                mv public new
+                git stash
 
-              git checkout origin/main
+                git checkout origin/main
 
-              build
-              mv public old
-              git checkout main
-              git stash pop
+                build
+                mv public old
+                git checkout main
+                git stash pop
 
-              ${pkgs.lib.getExe pkgs.meld} ./old ./new
-            '';
-            help = "Launch meld to diff between the `old` and `new` folders";
-          }
-          {
-            name = "new";
-            category = "development";
-            command = ''
-              set -euo pipefail
+                ${pkgs.lib.getExe pkgs.meld} ./old ./new
+              '';
+              help = "Launch meld to diff between the `old` and `new` folders";
+            }
+            {
+              name = "new";
+              category = "development";
+              command = ''
+                set -euo pipefail
 
-              function create_post() {
-                local title postslug postdate
-                if [ $# -lt 1 ]; then
-                  echo -e "\033[01;31mProviding a title is a must!\033[0m"
-                  return
-                else
-                  title="''${*}"
-                fi
-                postdate="$(date +"%Y-%m-%dT%H:%M:%S%:z")"
-                postslug="$(echo "$title" | tr -dc '[:alnum:][:space:]\n\r' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')"
-                [ -z "$postslug" ] && {
-                  echo -e "\033[01;31mProviding a title is a must!\033[0m"
-                  return
+                function create_post() {
+                  local title postslug postdate
+                  if [ $# -lt 1 ]; then
+                    echo -e "\033[01;31mProviding a title is a must!\033[0m"
+                    return
+                  else
+                    title="''${*}"
+                  fi
+                  postdate="$(date +"%Y-%m-%dT%H:%M:%S%:z")"
+                  postslug="$(echo "$title" | tr -dc '[:alnum:][:space:]\n\r' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')"
+                  [ -z "$postslug" ] && {
+                    echo -e "\033[01;31mProviding a title is a must!\033[0m"
+                    return
+                  }
+                  mkdir -p content/posts/"$postslug"
+                  printf "+++\ncategories = []\ndate = %s\nlastmod = %s\nsummary = \"\"\ndraft = true\nslug = \"%s\"\ntags = []\ntitle = \"%s\"\n+++\n" \
+                    "$postdate" "$postdate" "$postslug" "$title" >content/posts/"$postslug"/index.md
+                  echo "content/posts/$postslug created!"
                 }
-                mkdir -p content/posts/"$postslug"
-                printf "+++\ncategories = []\ndate = %s\nlastmod = %s\nsummary = \"\"\ndraft = true\nslug = \"%s\"\ntags = []\ntitle = \"%s\"\n+++\n" \
-                  "$postdate" "$postdate" "$postslug" "$title" >content/posts/"$postslug"/index.md
-                echo "content/posts/$postslug created!"
-              }
 
-              create_post "''${@}"
-            '';
-            help = "Create new post";
-          }
-        ];
-        env = [
-          {
-            name = "DEVSHELL_NO_MOTD";
-            value = 1;
-          }
-        ];
-      };
-    });
+                create_post "''${@}"
+              '';
+              help = "Create new post";
+            }
+          ];
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
+        };
+      }
+    );
 }
