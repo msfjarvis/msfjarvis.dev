f176166218a798e90699f1e6e8428f9474a5b1f4
Harsh Shandilya <me@msfjarvis.dev>
2026-05-01 10:12:27 +0530

    feat: RSS feeds for posts, notes, and weeknotes

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import type { APIContext } from 'astro';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+export async function GET(context: APIContext) {
+  const posts = await getCollection('posts', (p) => !p.data.deleted && !p.data.draft);
+  posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site!,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date,
+      description: post.data.summary,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
