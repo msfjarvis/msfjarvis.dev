33127a20b8eb6b352e4a4643411a475291dea6e0
Harsh Shandilya <me@msfjarvis.dev>
2026-05-01 18:05:05 +0530

    fix: move header styles to global.css to prevent FOUC from HMR race

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Harsh Shandilya';
+export const SITE_TITLE = 'msfjarvis.dev';
 export const SITE_DESCRIPTION = 'Systems Engineer at Cloudflare. Recovering Android developer, amateur Rustacean.';
 export const SITE_URL = 'https://msfjarvis.dev';
 export const AUTHOR_NAME = 'Harsh Shandilya';
