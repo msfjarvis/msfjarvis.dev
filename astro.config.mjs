b93f01558bd69be21282bba880c9fdc045f2722e
Harsh Shandilya <me@msfjarvis.dev>
2026-05-02 09:22:39 +0530

    feat: add Cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://msfjarvis.dev',
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
