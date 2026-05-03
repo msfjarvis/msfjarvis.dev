9200f2f4010aeeafd3889ccc2771590ef1dace47
Harsh Shandilya <me@msfjarvis.dev>
2026-05-03 12:52:38 +0530

    fix: try server output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://msfjarvis.dev',
+  output: 'server',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     imageService: 'compile',
