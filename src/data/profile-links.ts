3bea9cf276f3370f702ca2f09ba1f5c1fd72af67
Harsh Shandilya <me@msfjarvis.dev>
2026-05-22 10:09:19 +0530

    content: update some things

--- src/data/profile-links.ts
+++ src/data/profile-links.ts
@@ -11,13 +11,13 @@ export const profileLinks: ProfileLink[] = [
   {
     label: "mastodon",
     href: MASTODON_URL,
-    description: "follow along on the fediverse",
+    description: "follow me on the fediverse",
     icon: "simple-icons:mastodon",
   },
   {
     label: "git",
     href: GIT_SERVER_URL,
-    description: "self-hosted source and mirrors",
+    description: "all my code",
     icon: "simple-icons:forgejo",
   },
 ];
