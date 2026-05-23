11733ec64e5f69a4c187a02c4b20753edc5e8662
Harsh Shandilya <me@msfjarvis.dev>
2026-05-23 15:51:25 +0530

    refactor: make nav links data driven

---
+++ src/data/nav-links.ts
@@ -0,0 +1,31 @@
+export interface NavLinks {
+  label: string;
+  href: string;
+}
+
+export const navLinks: NavLinks[] = [
+  {
+    label: "posts",
+    href: "/posts",
+  },
+  {
+    label: "notes",
+    href: "/notes",
+  },
+  {
+    label: "weeknotes",
+    href: "/weeknotes",
+  },
+  {
+    label: "now",
+    href: "/now",
+  },
+  {
+    label: "about",
+    href: "/about",
+  },
+  {
+    label: "uses",
+    href: "/uses",
+  },
+];
