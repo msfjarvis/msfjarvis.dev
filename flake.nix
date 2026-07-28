7bc946ddc124a9b80589dfd85b8dfc87717abc14
Harsh Shandilya <me@msfjarvis.dev>
2026-07-28 22:43:02 +0530

    remove some stale nix commands

--- flake.nix
+++ flake.nix
@@ -73,12 +73,6 @@
           ];
           commands = [
             {
-              name = "build";
-              category = "deployment";
-              command = "pnpm run build";
-              help = "Build the site";
-            }
-            {
               name = "conv";
               category = "development";
               command = ''
@@ -89,12 +83,6 @@
                 done
               '';
               help = "Convert all PNGs to WebP";
-            }
-            {
-              name = "dev";
-              category = "development";
-              command = "pnpm run dev";
-              help = "Run the Hugo development server";
             }
 #             {
 #               name = "diffs";
@@ -137,36 +125,6 @@
 #               '';
 #               help = "Launch meld to diff between the `old` and `new` folders";
 #             }
-            {
-              name = "new";
-              category = "development";
-              command = ''
-                set -euo pipefail
-
-                function create_post() {
-                  local title postslug postdate
-                  if [ $# -lt 1 ]; then
-                    echo -e "\033[01;31mProviding a title is a must!\033[0m"
-                    return
-                  else
-                    title="''${*}"
-                  fi
-                  postdate="$(date +"%Y-%m-%dT%H:%M:%S%:z")"
-                  postslug="$(echo "$title" | tr -dc '[:alnum:][:space:]\n\r' | tr '[:upper:]' '[:lower:]' | sed 's/ /-/g')"
-                  [ -z "$postslug" ] && {
-                    echo -e "\033[01;31mProviding a title is a must!\033[0m"
-                    return
-                  }
-                  mkdir -p content/posts/"$postslug"
-                  printf "---\ncategories: []\ndate: '%s'\nlastmod: '%s'\nsummary: \"\"\ndraft: true\ntags: []\ntitle: \"%s\"\n--\n" \
-                    "$postdate" "$postdate" "$title" >content/posts/"$postslug"/index.md
-                  echo "content/posts/$postslug created!"
-                }
-
-                create_post "''${@}"
-              '';
-              help = "Create new post";
-            }
           ];
           env = [
             {
