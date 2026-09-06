74760558ab9a65f0868375ab0f658197f73baf34
Harsh Shandilya <me@msfjarvis.dev>
2026-09-06 20:42:14 +0530

    add sort-imports plugin for prettier

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,3 +1,7 @@
 export default {
-  plugins: ["prettier-plugin-astro"],
+  tabWidth: 2,
+  trailingComma: "all",
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: ["prettier-plugin-astro", "@trivago/prettier-plugin-sort-imports"],
 };
